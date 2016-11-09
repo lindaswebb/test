7ce0bf3b109b241a32603c411562f12709271f7f
lindaswebb <l_j@swipnet.se>
2016-11-09 15:38:09 +0100

    Add files via upload

---
+++ lilly.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html>
+<head>
+	<!--[if lt IE 9]>
+<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+    <meta charset="utf-8">
+    <title>Lilly - Millieville Bearded Collies</title>
+    <link href="style.css" rel="stylesheet" type="text/css">
+    <style type="text/css">
+    .stil2 {font-size: 9px}
+    </style>
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1" />
+    <meta name="keywords" content="bearded, collie, beardie, beardis, valpar, puppies, herding, obedience, agility, top dog,">
+    <meta name="description" content="Välkommen till Millieville Bearded Collies! Här kan du hitta information om Bearded Collien, men också en presentation av våra bearded collies med många bilder. Even in English.
+    Welcome to Millieville Bearded Collies! At this site you can find information about Bearded Collies but also about our bearded collies with many pictures.">
+    <meta http-equiv="imagetoolbar" content="no">
+</head>
+
+<body>
+
+	<div id="storbox">
+		<?php
+               include_once('meny.php');
+               include_once('topp.php');
+        ?>
+      <div id="main">
+		<article>
+          <h4 class="center">RLD N </h4>
+          <h1>Millieville Love Choice<br>
+          </h1>
+          
+          <p class="figure"><img src="bilder/Lilly-IMG_4922.jpg" alt="Lilly" width="900" height="600" border="1"><br>
+          </p>
+            <p class="center">HD B, ED: ua (0)</p>
+            <p>Då vi beslöt oss för att försöka få valpar efter Izzie stod en tikvalp högst upp på önskelistan. Det blev inte bara en utan fyra tikar och två hanar. Izzie gav dem en härlig start och tog hand om sina valpar väl. Sen var det där med att man skulle välja valp. Lilly, eller Strimman som hon kallades för först, gjorde det väldigt enkelt för mig. Hon valde mer eller mindre mig! </p>
+            <p>Lilly är som hund väldigt arbetsglad, mycket spring i benen och en väldigt glad hund i sig. Hon springer, precis som sin pappa och mamma, gärna med något i munnen. Just nu är det bara en massa grundträning som tränas. Vi har ingen brådska att komma ut på tävlingsbanorna. Lite av en slump hamnade vi på tävlingsbanorna då Lilly var 16 månader gammal i rallyns nybörjarklass. Trots att jag egentligen inte tyckte att Lilly var riktigt klar för tävling så skötte hon sig utmärkt! På sina tre första starter fick hon sin första titel, RLD N. En månad senare startade hon i fortsättningsklassen, som hon vann med 99 poäng av 100.</p>
+            <p>När vi är ute och går är bus med mamma Izzie som är höjdpunkten, frågan är vilken som tycker det är roligast - Izzie eller Lilly? Hon är väldigt lyhörd för mina signaler, så det är inga problem att ha henne lös. </p>
+            <p class="bildtext"><img src="bilder/lilly-izzie-IMG_0948.jpg" alt="Izzie och Lilly" width="700" height="468" class="bildtext"></p>
+		</article>
+      </div>
+
+</p>
+<script type="text/javascript">
+_uacct = "UA-4736086-1";
+urchinTracker();
+</script>
+<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+</script>  
+</body>
+</html>
